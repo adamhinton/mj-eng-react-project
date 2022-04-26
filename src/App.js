f5193823edfa371db30ff2ae1667d3a381425014
adamhinton <adam.d.hinton@gmail.com>
2022-04-26 16:32:34 -0400

    Insert h1 to App.js, confirm app starts and shows h1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Adam Hinton</h1>
+    </div>
+  );
 }
 
 export default App;
