8fae052e9b026744b25b8361a575ecae37cdd66f
adamhinton <adam.d.hinton@gmail.com>
2022-04-27 10:22:34 -0400

    add data-testid to h1 and Button in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,8 @@ import { Button } from "@mui/material";
 const Header = () => {
   return (
     <AppBar id="header-styling" data-testid="appBar">
-      <h1>Calorie Counter</h1>
-      <Button id="header-button" variant="contained">
+      <h1 data-testid="h1">Calorie Counter</h1>
+      <Button id="header-button" variant="contained" data-testid="header-btn">
         Clear All
       </Button>
     </AppBar>
