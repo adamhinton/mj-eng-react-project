bf8ad2e81beb214f85250849b447be86f2635629
adamhinton <adam.d.hinton@gmail.com>
2022-04-27 10:16:08 -0400

    sanity check in App.test

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,2 +1,8 @@
 import App from "../App";
 import { render, screen } from "@testing-library/react";
+
+test("Sanity Check", () => {
+  const twoPlusTwo = 2 + 2;
+  expect(twoPlusTwo).toStrictEqual(4);
+  expect(twoPlusTwo).not.toStrictEqual(5);
+});
