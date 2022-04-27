8f72ea2051f4ec38346b5a2a57776f378292765d
adamhinton <adam.d.hinton@gmail.com>
2022-04-27 10:46:42 -0400

    Add AddMealForm to App.js. Having problem where Header and AddMealForm overlap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
+import AddMealForm from "./Components/AddMealForm";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main></main>
+      <main>
+        <AddMealForm />
+      </main>
     </div>
   );
 }
