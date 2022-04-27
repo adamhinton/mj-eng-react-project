551aebf847742f2b7d530bfb2241ae7a73d9eaac
adamhinton <adam.d.hinton@gmail.com>
2022-04-27 10:23:23 -0400

    write Header.test:
    "[2] AppBar appears on screen"
    
    -It passes

--- src/Tests/Header.test.js
+++ src/Tests/Header.test.js
@@ -4,3 +4,10 @@ import { render, screen } from "@testing-library/react";
 test("[1] Renders without errors", () => {
   render(<Header />);
 });
+
+test("[2] AppBar appears on screen", () => {
+  render(<Header />);
+
+  const appBar = screen.getByTestId("appBar");
+  expect(appBar).toBeVisible();
+});
