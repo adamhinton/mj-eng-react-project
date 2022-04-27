213911a751e5ad93d795d0e3061d738e47dacc0b
adamhinton <adam.d.hinton@gmail.com>
2022-04-26 20:50:32 -0400

    successfully add Clear All button. Need to style it still

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import { AppBar } from "@mui/material";
+import { Button } from "@mui/material";
 
 const Header = () => {
   return (
     <AppBar>
       <h1>Calorie Counter</h1>
+      <Button variant="contained">Clear All</Button>
     </AppBar>
   );
 };
