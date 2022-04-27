4c6c727095d926f6da0119e3bd85692be56f0b2d
adamhinton <adam.d.hinton@gmail.com>
2022-04-26 21:17:04 -0400

    style header in to flex row

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import { Button } from "@mui/material";
 
 const Header = () => {
   return (
-    <AppBar>
+    <AppBar id="header-styling">
       <h1>Calorie Counter</h1>
       <Button variant="contained">Clear All</Button>
     </AppBar>
