2445d374e282bae34ad17780113b49e8fc9cfcaa
adamhinton <adam.d.hinton@gmail.com>
2022-04-28 01:07:57 -0400

    Write SM test: "[1] Renders without errors"

--- src/Tests/SingleMeal.test.js
+++ src/Tests/SingleMeal.test.js
@@ -1,2 +1,6 @@
 import SingleMeal from "../Components/SingleMeal";
 import { render, screen } from "@testing-library/react";
+
+test("[1] Renders without errors", () => {
+  render(<SingleMeal />);
+});
