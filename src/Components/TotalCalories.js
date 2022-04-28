10c617da3fb40852e9f2397036a2151b18f48e42
adamhinton <adam.d.hinton@gmail.com>
2022-04-28 00:05:55 -0400

    write test: "[2] Total Calories text appears on screen"

--- src/Components/TotalCalories.js
+++ src/Components/TotalCalories.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TotalCalories = () => {
-  return <h3>Total Calories: </h3>;
+  return <h3 data-testid="total-calories">Total Calories: </h3>;
 };
 
 export default TotalCalories;
--- src/Tests/TotalCalories.test.js
+++ src/Tests/TotalCalories.test.js
@@ -1,2 +1,14 @@
 import TotalCalories from "../Components/TotalCalories";
 import { render, screen } from "@testing-library/react";
+
+test("[1] Renders without errors", () => {
+  render(<TotalCalories />);
+});
+
+test("[2] Total Calories text appears on screen", () => {
+  render(<TotalCalories />);
+
+  const totalCaloriesText = screen.getByTestId("total-calories");
+
+  expect(totalCaloriesText).toBeVisible();
+});
