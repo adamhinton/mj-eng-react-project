42346873eaa79d3b778875e9d115395d6cf66536
adamhinton <adam.d.hinton@gmail.com>
2022-04-27 20:56:51 -0400

    add data-testids to form items

--- src/Components/AddMealForm.js
+++ src/Components/AddMealForm.js
@@ -8,21 +8,24 @@ export default function AddMealForm() {
   return (
     <form>
       <Paper id="add-meal-form">
-        <h2>Add Meal / Food Item</h2>
+        <h2 data-testid="form-h2">Add Meal / Food Item</h2>
 
         <section className="all-form-items">
           <div className="form-item-container">
-            <FormLabel>Meal</FormLabel>
-            <TextField placeholder="Add Item" />
+            <FormLabel data-testid="meal-label">Meal</FormLabel>
+            <TextField data-testid="meal-input" placeholder="Add Item" />
           </div>
 
           <div className="form-item-container">
-            <FormLabel>Calories</FormLabel>
-            <TextField placeholder="Add Calories" />
+            <FormLabel data-testid="calories-label">Calories</FormLabel>
+            <TextField
+              data-testid="calories-input"
+              placeholder="Add Calories"
+            />
           </div>
         </section>
       </Paper>
-      <Button id="submit-button" variant="contained">
+      <Button id="submit-button" data-testid="submit-btn" variant="contained">
         Add Meal
       </Button>
     </form>
