eef3dc72d5cc991257ab8075b42c91cf49cf0c3f
adamhinton <adam.d.hinton@gmail.com>
2022-04-27 20:19:13 -0400

    add form with h2 "Add Meal/Food Item"

--- src/Components/AddMealForm.js
+++ src/Components/AddMealForm.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+// import React, { useState } from "react";
+import { Button, Paper } from "@material-ui/core";
 
 export default function AddMealForm() {
   return (
-
+    <form>
+      <Paper id="add-meal-form">
+        <h2>Add Meal / Food Item</h2>
+      </Paper>
+    </form>
   );
 }
