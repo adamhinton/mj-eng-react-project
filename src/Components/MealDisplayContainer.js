02dc0bc56a662a4607503a2c49544424d7fae6d3
adamhinton <adam.d.hinton@gmail.com>
2022-04-28 00:25:19 -0400

    Tell MDC to map over dummy array and display SM.js

--- src/Components/MealDisplayContainer.js
+++ src/Components/MealDisplayContainer.js
@@ -1,5 +1,24 @@
+import SingleMeal from "./SingleMeal";
+
+const dummyArray = [
+  {
+    name: "Meal One",
+    count: 23112,
+  },
+  {
+    name: "Meal Two",
+    count: 223423,
+  },
+];
+
 const MealDisplayContainer = () => {
-  return <ul>fjdiaosfsdajio</ul>;
+  return (
+    <ul>
+      {dummyArray.map((item) => {
+        return <SingleMeal />;
+      })}
+    </ul>
+  );
 };
 
 export default MealDisplayContainer;
