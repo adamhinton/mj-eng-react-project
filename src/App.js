44f57e8e0d575596d4bf581b6385de533237e71f
adamhinton <adam.d.hinton@gmail.com>
2022-04-28 13:27:06 -0400

    wrap App.js components in ThingsProvider with dummy data

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Header from "./Components/Header";
 import AddMealForm from "./Components/AddMealForm";
 import TotalCalories from "./Components/TotalCalories";
 import MealDisplayContainer from "./Components/MealDisplayContainer";
-import ThingsProvider
+import { ThingsProvider } from "./Context/MyContext";
+
+const things = [{ name: "Dummy data" }, { name: "More dummy data" }];
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        <AddMealForm />
-        <TotalCalories />
-        <MealDisplayContainer />
-      </main>
+      <ThingsProvider value={things}>
+        <Header />
+        <main>
+          <AddMealForm />
+          <TotalCalories />
+          <MealDisplayContainer />
+        </main>
+      </ThingsProvider>
     </div>
   );
 }
