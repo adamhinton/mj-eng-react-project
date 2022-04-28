82152c95f68c08c56bf01a1dbcda452189971508
adamhinton <adam.d.hinton@gmail.com>
2022-04-28 13:33:12 -0400

    delete MDC dummy data and comment out c.log

--- src/Components/MealDisplayContainer.js
+++ src/Components/MealDisplayContainer.js
@@ -2,21 +2,10 @@ import SingleMeal from "./SingleMeal";
 import ThingsContext from "../Context/MyContext";
 import { useContext } from "react";
 
-const dummyArray = [
-  {
-    mealName: "Meal One",
-    count: 23112,
-  },
-  {
-    mealName: "Meal Two",
-    count: 223423,
-  },
-];
-
 const MealDisplayContainer = () => {
   const things = useContext(ThingsContext);
 
-  console.log("things.mealInfo:", things.mealInfo);
+  // console.log("things.mealInfo:", things.mealInfo);
 
   return (
     <ul>
