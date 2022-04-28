60630a174fd07943ad43c97eeb9f37d5906a3333
adamhinton <adam.d.hinton@gmail.com>
2022-04-28 01:14:10 -0400

    write SM.test:
    "[2] Display correct meal name and calorie count"

--- src/Tests/SingleMeal.test.js
+++ src/Tests/SingleMeal.test.js
@@ -4,3 +4,13 @@ import { render, screen } from "@testing-library/react";
 test("[1] Renders without errors", () => {
   render(<SingleMeal />);
 });
+
+test("[2] Display correct meal name and calorie count", () => {
+  render(<SingleMeal mealName="Meal One" count={300} />);
+
+  const mealNameText = screen.getByText(/meal one/i);
+  const mealCountText = screen.getByText(/300/);
+
+  expect(mealNameText).toBeVisible();
+  expect(mealCountText).toBeVisible();
+});
