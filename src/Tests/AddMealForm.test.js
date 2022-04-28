f0737bcdcd3cc692fe157cb85dcd13a6920c9b1b
adamhinton <adam.d.hinton@gmail.com>
2022-04-27 20:58:09 -0400

    write AddMealForm.test:  "[2] Meal input and label appear on screen"

--- src/Tests/AddMealForm.test.js
+++ src/Tests/AddMealForm.test.js
@@ -4,3 +4,13 @@ import { render, screen } from "@testing-library/react";
 test("[1] Renders without errors", () => {
   render(<AddMealForm />);
 });
+
+test("[2] Meal input and label appear on screen", () => {
+  render(<AddMealForm />);
+
+  const mealLabel = screen.getByTestId("meal-label");
+  const mealInput = screen.getByTestId("meal-input");
+
+  expect(mealLabel).toBeVisible();
+  expect(mealInput).toBeVisible();
+});
