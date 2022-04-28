48a29194edc35e4bae8c552abc6dbeea26ae4899
adamhinton <adam.d.hinton@gmail.com>
2022-04-27 20:53:11 -0400

    write test: [1] Renders without errors

--- src/Tests/AddMealForm.test.js
+++ src/Tests/AddMealForm.test.js
@@ -1,2 +1,6 @@
 import AddMealForm from "../Components/AddMealForm";
 import { render, screen } from "@testing-library/react";
+
+test("[1] Renders without errors", () => {
+  render(<AddMealForm />);
+});
