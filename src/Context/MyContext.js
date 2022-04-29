08392ade933bfcd9fccc0e4b868aa0fadbe7defd
adamhinton <adam.d.hinton@gmail.com>
2022-04-29 10:52:16 -0400

    del unneeded comments

--- src/Context/MyContext.js
+++ src/Context/MyContext.js
@@ -1,16 +1,3 @@
-// import React from "react";
-
-// const things = {
-//   mealInfo: [
-//     { mealName: "Dummy data", count: 1234 },
-//     { mealName: "More dummy data", count: 324325 },
-//   ],
-// };
-
-// const ThingsContext = React.createContext(things);
-// export const ThingsProvider = ThingsContext.Provider;
-
-// export default ThingsContext;
 import { createContext } from "react";
 
 const ThingsContext = createContext();
