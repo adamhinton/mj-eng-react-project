6a8853cf9d39a563e457cb782293b78f09208296
adamhinton <adam.d.hinton@gmail.com>
2022-04-29 19:23:48 -0400

    create clearAllItemsClick in Header.js -Add it to Clear ALl button as onClick
    -It works. Yay!

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,23 @@ import { AppBar } from "@mui/material";
 import { Button } from "@mui/material";
 import ItemCtrl from "../CrudFunctions/ItemCtrl";
 import StorageCtrl from "../CrudFunctions/StorageCtrl";
+import { useContext } from "react";
+import ThingsContext from "../Context/MyContext";
 
 const Header = () => {
+  const { myThings, setMyThings } = useContext(ThingsContext);
+
   return (
     <AppBar id="header-styling" data-testid="appBar">
       <h1 data-testid="header-h1">Calorie Counter</h1>
-      <Button id="header-button" variant="contained" data-testid="header-btn">
+      <Button
+        id="header-button"
+        variant="contained"
+        data-testid="header-btn"
+        onClick={() => {
+          clearAllItemsClick(setMyThings);
+        }}
+      >
         Clear All
       </Button>
     </AppBar>
@@ -16,4 +27,9 @@ const Header = () => {
 
 export default Header;
 
-const clearAllItemsClick = () => {};
+const clearAllItemsClick = (setMyThings) => {
+  ItemCtrl.clearAllItems();
+  StorageCtrl.clearItemsFromStorage();
+  console.log("clearing");
+  setMyThings([]);
+};
