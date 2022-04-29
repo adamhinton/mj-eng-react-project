b086df2ba264bf7c8eb49be228014bc34544800c
adamhinton <adam.d.hinton@gmail.com>
2022-04-29 18:57:17 -0400

    del comments

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import Header from "./Components/Header";
 import AddMealForm from "./Components/AddMealForm";
 import TotalCalories from "./Components/TotalCalories";
 import MealDisplayContainer from "./Components/MealDisplayContainer";
-// import { ThingsProvider } from "./Context/MyContext";
 import ThingsContext from "./Context/MyContext";
-// import { useContext } from "react";
 import { useState } from "react";
 import StorageCtrl from "./CrudFunctions/StorageCtrl";
 
 const things = StorageCtrl.getItemsFromStorage();
-console.log("things:", things);
 
 function App() {
   const [myThings, setMyThings] = useState(things);
