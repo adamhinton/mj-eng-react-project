c5b2ed7612a386add1c4a7bc513d28f43a8642ee
adamhinton <adam.d.hinton@gmail.com>
2022-04-29 21:39:12 -0400

    del unneeded variables and c.logs

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import Header from "./Components/Header";
 import AddMealForm from "./Components/AddMealForm";
 import TotalCalories from "./Components/TotalCalories";
 import MealDisplayContainer from "./Components/MealDisplayContainer";
-import ThingsContext from "./Context/MyContext";
-import { useState } from "react";
-import StorageCtrl from "./CrudFunctions/StorageCtrl";
 import { ThingsProvider } from "./Context/MyContext";
 
-const things = StorageCtrl.getItemsFromStorage();
-
 function App() {
-  // const [myThings, setMyThings] = useState(things);
-
   return (
     <div className="App">
       <ThingsProvider>
