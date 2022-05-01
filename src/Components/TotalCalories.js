f273608545147b42d5a96d31b80b40e0d3a04340
adamhinton <adam.d.hinton@gmail.com>
2022-05-01 02:59:53 -0400

    Subscribed TotalCalories.js to context so it now updates when a meal is added

--- src/Components/TotalCalories.js
+++ src/Components/TotalCalories.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import ItemCtrl from "../CrudFunctions/ItemCtrl";
+import ThingsContext from "../Context/MyContext";
 
 const TotalCalories = () => {
+  useContext(ThingsContext);
+
   const totalCalories = ItemCtrl.getTotalCalories();
 
   return <h3 data-testid="total-calories">Total Calories: {totalCalories} </h3>;
