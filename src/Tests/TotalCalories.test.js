b3a04c61b73977ec8d30683830becd7ccb4228e2
adamhinton <adam.d.hinton@gmail.com>
2022-05-01 18:46:05 -0400

    update new class name in TotalCalories.test

--- src/Tests/TotalCalories.test.js
+++ src/Tests/TotalCalories.test.js
@@ -8,7 +8,7 @@ test("[1] Renders without errors", () => {
 test("[2] Total Calories text appears on screen", () => {
   render(<TotalCalories />);
 
-  const totalCaloriesText = screen.getByTestId("total-calories");
+  const totalCaloriesText = screen.getByTestId("total-calories-h3");
 
   expect(totalCaloriesText).toBeVisible();
 });
