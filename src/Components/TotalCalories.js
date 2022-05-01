97f53db7d9be430de7e0c2e07f1a47e8481b11ab
adamhinton <adam.d.hinton@gmail.com>
2022-05-01 03:20:45 -0400

    TotalCalories.js now works after editing and deleting single meals

--- src/Components/TotalCalories.js
+++ src/Components/TotalCalories.js
@@ -1,11 +1,18 @@
 import React, { useContext } from "react";
 import ItemCtrl from "../CrudFunctions/ItemCtrl";
 import ThingsContext from "../Context/MyContext";
+import StorageCtrl from "../CrudFunctions/StorageCtrl";
 
 const TotalCalories = () => {
+  // console.log("context changing in TC:");
   useContext(ThingsContext);
 
-  const totalCalories = ItemCtrl.getTotalCalories();
+  const currentData = StorageCtrl.getItemsFromStorage();
+  let totalCalories = 0;
+
+  currentData.forEach((item) => {
+    totalCalories += item.calories;
+  });
 
   return <h3 data-testid="total-calories">Total Calories: {totalCalories} </h3>;
 };
