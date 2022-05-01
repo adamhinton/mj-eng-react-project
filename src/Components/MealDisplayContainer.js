544f96b5aa2c7e50535821e23a23d41fffa4c37e
adamhinton <adam.d.hinton@gmail.com>
2022-05-01 18:22:14 -0400

    comments in MDC.js

--- src/Components/MealDisplayContainer.js
+++ src/Components/MealDisplayContainer.js
@@ -1,8 +1,11 @@
+// This component is pretty simple. It takes in from Context the list of meals that the user has added, and maps over it, displaying a SingleMeal component for each.
+
 import SingleMeal from "./SingleMeal";
 import ThingsContext from "../Context/MyContext";
 import { useContext } from "react";
 
 const MealDisplayContainer = () => {
+  // myThings is the array of all meals, taken from context
   const { myThings } = useContext(ThingsContext);
 
   return (
