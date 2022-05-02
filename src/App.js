54538f918daf6619810f53f64326dea1842eba8f
adamhinton <adam.d.hinton@gmail.com>
2022-05-02 14:45:40 -0400

    change ThingsProvider to MealsProvider in App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Header from "./Components/Header";
 import AddMealForm from "./Components/AddMealForm";
 import TotalCalories from "./Components/TotalCalories";
 import MealDisplayContainer from "./Components/MealDisplayContainer";
-import { ThingsProvider } from "./Context/MyContext";
+import { MealsProvider } from "./Context/MyContext";
 
 function App() {
   return (
     <div className="App">
-      <ThingsProvider>
+      <MealsProvider>
         <Header />
         <main>
           <AddMealForm />
           <TotalCalories />
           <MealDisplayContainer />
         </main>
-      </ThingsProvider>
+      </MealsProvider>
     </div>
   );
 }
